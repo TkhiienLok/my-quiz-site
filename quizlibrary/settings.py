5435a70a9b3ec3d0a35781e1eae43bddce9c33a2
TkhiienLok <tkhiien@gmail.com>
2020-05-27 19:49:45 +0300

    Removed redundant debug variable

--- quizlibrary/settings.py
+++ quizlibrary/settings.py
@@ -34,9 +34,6 @@ if os.path.isfile(env_file):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost', 'lok-quiz-platform.herokuapp.com']
 
 
